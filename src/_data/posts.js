b51a3de1b2796b25548d30bdfed89c47e339ef1b
Jaye Hackett <joshua.hackett93@gmail.com>
2020-12-13 21:41:49 +0000

    Update and rename courses.js to posts.js

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch")
 
 module.exports = async () => {
-    const res = await fetch("https://opportunities.hackney.gov.uk/wp-json/wp/v2/course")
+    const res = await fetch("https://www.rollingstone.com/wp-json/wp/v2/posts")
     return await res.json()
 }
