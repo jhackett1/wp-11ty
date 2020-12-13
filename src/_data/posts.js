abbcd53ba8378b9f3df5cfccfa6c38b871ec2334
Jaye Hackett <joshua.hackett93@gmail.com>
2020-12-13 21:48:12 +0000

    Update posts.js

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,5 +1,9 @@
 const fetch = require("node-fetch")
 
+// Fetch the most recent 10 posts
+// If we want more, we can loop through requests, up to 100 at a time, and append them to an array
+// https://developer.wordpress.org/rest-api/using-the-rest-api/pagination/
+
 module.exports = async () => {
     const res = await fetch("https://www.rollingstone.com/wp-json/wp/v2/posts")
     return await res.json()
